bb4eb07823a1ec56d5d3c167f43b478f7bfad68d
zephyrfalcon <zephyrfalcon@gmail.com>
2010-05-24 23:33:41 -0400

    start Tk graphics

---
+++ graphics.scm
@@ -0,0 +1,16 @@
+;; graphics.scm
+
+(use tk)
+
+(define (init-graphics)
+  (start-tk))
+
+;; for now, there's only one canvas, which can be accessed globally.
+(define *canvas* #f)
+
+(define (make-canvas width height bgcolor)
+  (set! *canvas* (tk 'create-widget 'canvas
+                     #:height height #:width width #:bg bgcolor))
+  (tk/pack *canvas* #:expand #t #:fill 'both))
+
+
